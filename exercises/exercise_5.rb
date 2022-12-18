8f36f16e106f275c1bd5674f4ab0c510f0de142d
Mohamed Khalaf <mkhalaf2211@gmail.com>
2022-12-18 00:18:48 +0000

    Calculate sum and average of annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@sum =Store.sum(:annual_revenue)
+puts "Annual revenue Sum: #{@sum}"
+
+@average = @sum / Store.count
+puts "Annual revenue Average: #{@average}"
+
+@rich_stores = Store.where(annual_revenue: 1000000..).count
+puts "Rich Stores: #{@rich_stores}"
+
+
+
