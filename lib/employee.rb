3dfec5951a133e14f92f42a6131adb413e1c6703
Mohamed Khalaf <mkhalaf2211@gmail.com>
2022-12-18 00:20:28 +0000

    add validation

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,6 @@
 class Employee < ActiveRecord::Base
-
+  belongs_to :store, optional:false
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, presence: true, numericality: {only_integer: true, in: 40..200}
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,13 @@
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, presence: true, length: {minimum: 3}
+  validates :annual_revenue, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
+  validate :no_apparel_defined
+
+  def no_apparel_defined
+    if !mens_apparel && !womens_apparel
+      errors.add("Please specify an apparel it cannot be empty")
+    end
+  end
 
 end
